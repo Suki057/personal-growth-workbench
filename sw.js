fdcbc6d47005b7d813159059b4acf27cef46c9a2
Suki057 <Suki057@users.noreply.github.com>
2026-08-01 21:09:40 +0800

    SW 缓存升 v3 并 skipWaiting，确保手机端即时更新

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'wb-growth-v2';
+const CACHE_NAME = 'wb-growth-v3';
 const ASSETS = [
   './index.html',
   './manifest.webmanifest',
@@ -9,6 +9,7 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => cache.addAll(ASSETS))
@@ -20,7 +21,7 @@ self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) =>
       Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    )
+    ).then(() => self.clients.claim())
   );
 });
 
